d6f4eb4f6c21479aa7eae1ad668630e0fd8c2940
nummer14 <nummer14@naver.com>
2025-07-04 09:53:00 +0900

    lombok annotation

--- src/main/java/com/example/lombok_demo/LombokDemoApplication.java
+++ src/main/java/com/example/lombok_demo/LombokDemoApplication.java
@@ -1,33 +1,14 @@
 package com.example.lombok_demo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 class Memo {
 	private int id;
 	private String title;
 	private String content;
-
-	public int getId() {
-		return id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
 }
 
 
@@ -36,6 +17,6 @@ public class LombokDemoApplication {
 	public static void main(String[] args) {
 		Memo memo = new Memo();
 		memo.setTitle("Hello, Lombok!");
-		System.out.println(memo.getTitle());
+		assert "Hello, Lombok!".equals(memo.getTitle());
 	}
 }

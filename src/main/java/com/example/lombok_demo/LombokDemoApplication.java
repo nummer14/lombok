4ddac706884ad6d03050f00d61362c417a312215
nummer14 <nummer14@naver.com>
2025-07-04 10:47:58 +0900

    ds

--- src/main/java/com/example/lombok_demo/LombokDemoApplication.java
+++ src/main/java/com/example/lombok_demo/LombokDemoApplication.java
@@ -6,17 +6,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@ToString(includeFieldNames = false)
 class DailySales {
 	private LocalDate saleDate;
 	private BigDecimal totalAmount;
 	private int orderCount;
-
-	@Override
-	public String toString() {
-		return "DailySales(saleDate=" + saleDate
-				+ ", totalAmount=" + totalAmount
-				+ ", orderCount=" + orderCount + ")";
-	}
 }
 
 
@@ -24,6 +18,6 @@ public class LombokDemoApplication {
 	public static void main(String[] args) {
 		DailySales ds = new DailySales(LocalDate.of(2025, 7, 4), new BigDecimal("10000.00"), 3);
 
-		System.out.println(ds.toString());;
+		System.out.println(ds);
 	}
 }
